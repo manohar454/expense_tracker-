11bd93a9f856a9b173b3cef199cf5b0c101d5586
manohar454 <maheshpusala16@gmail.com>
2026-01-29 15:29:07 +0530

    Update models.py

--- backend/models.py
+++ backend/models.py
@@ -1,1 +1,12 @@
+from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
+
+db = SQLAlchemy()
+
+class Expense(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    amount = db.Column(db.Float, nullable=False)
+    category = db.Column(db.String(50), nullable=False)
+    description = db.Column(db.String(200))
+    date = db.Column(db.Date, default=datetime.utcnow)
 
